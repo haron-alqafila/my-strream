32017934ad72a963b28cc99070a1e53b1d01509c
haron-alqafila <55305248+haron-alqafila@users.noreply.github.com>
2019-09-28 15:20:24 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ client.on('ready', () => {
 });
 
 
-const developers = ["307486799645900800"]
+const developers = ["579762663240761527"]
 const adminprefix = ".";
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
